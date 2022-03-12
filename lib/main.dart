dd6b555269aafc5a2d394693479fc705dc959c19
iAbhishekPatil <abhishek.patil@joflee.com>
2022-03-12 19:56:29 +0530

    Padding is now dynmic based on desktop or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,12 +25,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  bool get isDesktop =>
+      kIsWeb || (Platform.isLinux || Platform.isMacOS || Platform.isWindows);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 400),
+          padding: EdgeInsets.symmetric(horizontal: isDesktop ? 400 : 20),
           child: SearchBar(),
         ),
       ),
